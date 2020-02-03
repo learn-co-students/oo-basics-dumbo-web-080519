409625d7932fc3f71125f254f3949b6798a83911
Zermina Ejaz <zerminaejaz@gmail.com>
2020-02-02 23:06:04 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -7,36 +7,7 @@ class Book
         @title = title
     end
 
-    # def title
-    #     @title
-    # end
-
-    # def author=(aauthor)
-    #     @author = aauthor
-    # end
-    
-    # def author
-    #     @author
-    # end
-
-    # def page_count=(page_count)
-    #     @page_count = page_count
-    # end
-
-    # def page_count
-    #     @page_count
-    # end
-
-    # def genre=(genre)
-    #     @genre = genre
-    # end
-
-    # def genre
-    #     @genre
-    # end
-
     def turn_page
-
         puts "Flipping the page...wow, you read fast!"
     end
 
